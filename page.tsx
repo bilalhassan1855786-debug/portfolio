fecfcaa87d9f9d057b0123927a2f5f78a2d4fbac
BILAL HASSAN <bilalhassan.1855786@gmail.com>
2026-04-08 03:08:38 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,16 @@
+// src/app/about/page.tsx
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">About Me</h2>
+      <p className="text-gray-700">
+          Driven by a passion for AI and innovation, I design and develop smart, scalable web applications using Next.js, Tailwind CSS, and TypeScript. My goal is to merge automation with elegant design to create seamless and high-quality user experiences.
+      </p>
+    </div>
+  )
+}
+
+export default About
+
